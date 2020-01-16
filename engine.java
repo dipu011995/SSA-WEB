69264ec4a09908f728ce20c2d0e1ec2d5835df49
pankaj <pankaj52dipu@gmail.com>
2020-01-16 12:45:19 +0530

    commited

--- engine.java
+++ engine.java
@@ -4,5 +4,10 @@ public class Engine{
  }
  public void m3(){
    System.out.println("kumar");
+ }public void m4(){
+   System.out.println("pp");
+ }
+ public void m5(){
+   System.out.println("kumar");
  }
 }
